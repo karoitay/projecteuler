d55c6b075589e64608976383ab7ac6418f3d5bc4
Itay Karo <karoitay@gmail.com>
2013-05-16 20:35:18 +0300

    solved problem 76

---
+++ python/p76.py
@@ -0,0 +1,29 @@
+import numpy
+
+_MAX = 100
+
+A = numpy.zeros((_MAX + 1, _MAX + 1), dtype=numpy.int)
+
+def get_count(n, m):
+  """How many options can we create n as
+     as a sum of terms when all terms <= m"""
+  if n == 1 or m == 1:
+    return 1
+  if m > n:
+    return A[n][n]
+  if n == m:
+    return 1 + A[n][n - 1]
+
+  return A[n - m][m] + A[n][m - 1]
+  count = get_count(n - m, m) # for m + ...
+  for i in xrange(1, m):
+    # for the form i + ..., where i < m
+    c = get_count(n - i, min(m, i))
+    count = count + c
+  return count
+
+for i in xrange(1, _MAX + 1):
+  for j in xrange(1, _MAX + 1):
+    A[i][j] = get_count(i, j)
+print A[_MAX][_MAX] - 1
+
