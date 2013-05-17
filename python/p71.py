f704550e5f3918cce4db73eaf1804927091a4932
Itay Karo <karoitay@gmail.com>
2013-05-17 16:06:43 +0300

    make 71 even more effective by only computing gcd at the end

--- python/p71.py
+++ python/p71.py
@@ -1,11 +1,11 @@
 MAX = 1000000
 
-def arecoprime(a, b):
+def gcd(a, b):
   while a != b:
     a_even = a % 2 == 0
     b_even = b % 2 == 0
     if a_even and b_even:
-      return False
+      a, b = a / 2, b / 2
     elif a_even:
       a = a / 2
     elif b_even:
@@ -14,16 +14,22 @@ def arecoprime(a, b):
       a = (a - b) / 2
     else:
       b = (b - a) / 2
-  return a == 1
+  return a
 
 max_num = 0
 max_val = 0
 max_denom = 0
 for denom in xrange(3, MAX + 1):
   num = int((3 * denom - 1) / 7)
-  if arecoprime(num, denom) and float(num) / denom > max_val:
+  if float(num) / denom > max_val:
     max_val = float(num) / denom
     max_num = num
     max_denom = denom
+
+# now to reduced form
+d = gcd(max_num, max_denom)
+while d != 1:
+  max_num, max_denom = max_num / d, max_denom / d
+  d = gcd(max_num, max_denom)
 print max_num, "/", max_denom
 
