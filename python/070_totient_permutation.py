4003bc78911fa51f5d96d575fb682520a6b97cea
Itay Karo <karoitay@gmail.com>
2013-05-19 20:12:36 +0300

    solved 70

---
+++ python/070_totient_permutation.py
@@ -0,0 +1,37 @@
+import math
+MAX = 10**7
+HIGH_BOUND = int(math.sqrt(MAX) * 2)
+
+def permutations(n1, n2):
+  l1, l2 = list(str(n1)), list(str(n2))
+  if len(l1) != len(l2):
+    return False
+  for c in l1:
+    if not c in l2:
+      return False
+    l2.remove(c)
+  return len(l2) == 0
+
+primes = [None] * HIGH_BOUND
+primes[0] = False
+primes[1] = False
+for i in xrange(2, HIGH_BOUND):
+  if primes[i] == None:
+    primes[i] = True
+    for j in xrange(2*i, HIGH_BOUND, i):
+      primes[j] = False
+primes = [i for i in xrange(2, HIGH_BOUND) if primes[i]]
+
+result = 0
+max_found = 0
+for p1 in primes:
+  for p2 in primes:
+    if p1 == p2 or p1 * p2 >= MAX:
+      continue
+    n, phi_n = p1 * p2, (p1 - 1) * (p2 - 1)
+    if not permutations(n, phi_n):
+      continue
+    if float(phi_n) / n > max_found:
+      max_found = float(phi_n) / n
+      result = n
+print result
