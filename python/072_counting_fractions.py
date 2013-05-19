ab554eef296e0bc57cbf3739c5e79b5c28cb8d26
Itay Karo <karoitay@gmail.com>
2013-05-19 20:36:44 +0300

    solved 72

---
+++ python/072_counting_fractions.py
@@ -0,0 +1,20 @@
+MAX = 1000000
+
+def phi(i, factors):
+  for f in factors[i]:
+    i *= ((f - 1) / float(f))
+  return i
+
+factors = {}
+for i in xrange(2, MAX + 1):
+  if i in factors:
+    continue
+  for j in xrange(i, MAX + 1, i):
+    factors_j = factors.get(j, [])
+    factors_j.append(i)
+    factors[j] = factors_j
+
+result = 0
+for i in xrange(2, MAX + 1):
+  result += phi(i, factors)
+print int(result)
