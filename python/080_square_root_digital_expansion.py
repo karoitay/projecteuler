6a47fc97669af2172c4b225884f060322abecec9
Itay Karo <karoitay@gmail.com>
2013-05-25 04:24:16 +0300

    80 solved

---
+++ python/080_square_root_digital_expansion.py
@@ -0,0 +1,12 @@
+from decimal import *
+
+MAX = 100
+INTEGRAL_ROOTS = set([1, 4, 9, 16, 25, 36, 49, 64, 81])
+getcontext().prec = 105
+result = 0
+for i in xrange(2, 100):
+  if not i in INTEGRAL_ROOTS:
+    s = str(Decimal(i).sqrt()).split('.')
+    result += int(s[0]) + sum([int(c) for c in s[1][0:99]])
+print result
+
