d763b66123f1df4f412ed416112f4c88b5d4e2a3
Itay Karo <karoitay@gmail.com>
2013-10-05 18:07:43 +0300

    problem 3 solved

---
+++ python/003_largest_prime_factor.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+from utils.primes import prime_factors
+print max(prime_factors(600851475143))
--- python/utils/primes.py
+++ python/utils/primes.py
@@ -17,3 +17,22 @@ def getprimes(n):
       primes[j] = False
   return primes
 
+def prime_factors(n):
+  """Computes the prime factors of n
+     This is a naive inefficient implementation.
+
+  Args:
+    n: the number to compute the prime factors for
+
+  Return:
+    A set with the prime factors of n
+  """
+  prime_factors = set()
+  d = 1
+  while n != 1:
+    d += 1
+    if n % d == 0:
+      prime_factors.add(d)
+      while n % d == 0:
+        n /= d
+  return prime_factors
